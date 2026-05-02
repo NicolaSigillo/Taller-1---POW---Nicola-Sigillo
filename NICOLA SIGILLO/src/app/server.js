d4727922c88eec900d00d3891ec429d62e9b77d4
Nicola <nicolasigillo2@gmail.com>
2026-05-02 13:48:28 -0400

    Base inicial del servidor (codigo que hicimos en clase)

---
+++ NICOLA SIGILLO/src/app/server.js
@@ -0,0 +1,29 @@
+import cors from 'cors';
+import express, { json } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import indexRouter from '../routes/index.routes.js';
+
+//SETINGS
+const app = express();
+app.set("port", 3000);
+
+//MIDDLEWARES
+app.use(morgan("dev"));
+app.use(json());
+app.use(
+    cors({
+        "origin": "*"
+    })
+);
+app.use(helmet());
+
+//ROUTES
+app.get("/", (req, res) => {
+    res.status(200).send({
+        msg: "Server Runnning:"
+    })
+});
+app.use("/api", indexRouter);
+export default app;
